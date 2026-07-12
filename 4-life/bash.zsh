9762742f443749d22f7eb6464596dbe936a243d5
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-12 22:45:40 +0100

    Create bash.zsh

---
+++ 4-life/bash.zsh
@@ -0,0 +1,10 @@
+# 1. Clone the repository
+git clone https://github.com
+cd 4-life
+
+# 2. Install dashboard dependencies
+npm install
+
+# 3. Navigate to the adversarial directory and set up Python dependencies
+cd adversarials
+pip install -r requirements.txt  # If available, or manually install standard data stacks like numpy/scipy if prompted
